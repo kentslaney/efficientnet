d789c518ae05224e86dbd67f9b230eb612f7f410
Kent Slaney <kent@slaney.org>
2020-09-06 13:01:44 -0400

    initial commit

---
+++ border.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+
+expand = lambda r: lambda x: (x,) * r if tf.rank(x) == 0 else x
+
+class Border(tf.Module):
+    default = None
+
+    def __init__(self, width, stride=1, rank=2, trainable=True, name=None):
+        super().__init__(name=name)
+        assert self.default is not None and tf.rank(self.default) == 0
+        self.rank, self.trainable = rank, trainable
+        width, stride = map(expand(rank), (width, stride))
+        self.stride = tf.convert_to_tensor(stride)
+        self.values = [[self.initialize(size, i, axis, bool(end))
+                        for end, size in enumerate(((i - 1) // 2, i // 2))]
+                       for axis, i in enumerate(width)]
+
+    # @tf.function(input_signature=[tf.TensorSpec([None], tf.int32)])
+    def __call__(self, shape):
+        tf.debugging.assert_equal(tf.shape(shape), (self.rank,))
+        return self._build(shape, [])
+
+    def _build(self, shape, built):
+        axis = len(built)
+        if axis == self.rank:
+            built = tf.meshgrid(*built, indexing="ij")
+            res = built[0]
+            for i in built[1:]:
+                res = self.compose(res, i)
+            return res
+
+        size, stride = shape[axis], self.stride[axis]
+        start, end = self.values[axis]
+        ssize, esize = map(tf.size, (start, end))
+        start, end = self.conv(start, False), self.conv(end, True)
+        if ssize + esize <= size:
+            middle = tf.repeat(self.default, size - ssize - esize)
+        elif tf.math.maximum(ssize, esize) <= size:
+            msize = ssize + esize - size
+            middle = self.overlap(start[-msize:], end[:msize])
+            start, end = start[:-msize], end[msize:]
+        else:
+            middle = self.overlap(start[:size], end[esize - size:])
+            start, end = tf.zeros((0,)), tf.zeros((0,))
+
+        reps = (size + stride - 1) // stride
+        pad = tf.nn.relu((reps - 1) * stride + ssize + esize + 1 - size)
+        offset = ssize - pad // 2
+        ssize, msize = map(tf.size, (start, middle))
+        start = start[offset::stride]
+        middle = middle[(stride + offset - ssize) % stride::stride]
+        end = end[(stride + offset - ssize - msize) % stride::stride]
+
+        return tf.concat((
+            self._build(shape, built + [start]),
+            self._build(shape, built + [middle]),
+            self._build(shape, built + [end]),
+        ), len(built))
+
+    # @tf.function(input_signature=[
+    #     tf.TensorSpec([None]), tf.TensorSpec([], tf.bool)])
+    def conv(self, inp, end):
+        out = inp
+        if tf.size(inp) > 1:
+            for i in tf.range(1, tf.size(inp)):
+                j = ((inp, i), (out, 0))
+                (left, offset), (right, lim) = j if end else j[::-1]
+                update = self.compose(right[i:], left[:-i])
+                ind = tf.range(offset, tf.size(inp) - lim)[:, tf.newaxis]
+                out = tf.tensor_scatter_nd_update(out, ind, update)
+        return out
+
+    def initialize(self, size, width, axis, end):
+        raise NotImplementedError()
+
+    @classmethod
+    def compose(_, a, b):
+        raise NotImplementedError()
+
+    @classmethod
+    def overlap(_, a, b):
+        raise NotImplementedError()
+
+class BorderReweight(Border):
+    default = tf.constant(1.)
+
+    @tf.Module.with_name_scope
+    def initialize(self, size, width, axis, end):
+        name = f"border_reweight_axis{axis}_{'end' if end else 'start'}"
+        res = tf.range(width - size, width)[::-1 if end else 1]
+        res = tf.cast((res + 1) / res, tf.float32)
+        return tf.Variable(res, self.trainable, name=name)
+
+    @classmethod
+    def compose(_, a, b):
+        return a * b
+
+    @classmethod
+    def overlap(_, a, b):
+        return a * b / (a + b - a * b)
+
+# double counts the corners, which is unavoidable with O(n) parameters for an
+# n by n kernel
+# Proof: if you conv a kernel with ones, you can recover all the kernel weights
+# by subtracting adjacent values towards the relevant corner therefore you need
+# O(n^2) information to suplement the lost corners
+class BorderOffset(Border):
+    default = tf.constant(0.)
+
+    @tf.Module.with_name_scope
+    def initialize(self, size, width, axis, end):
+        name = f"border_bias_axis{axis}_{'end' if end else 'start'}"
+        return tf.Variable(tf.zeros((size,)), self.trainable, name=name)
+
+    @classmethod
+    def compose(_, a, b):
+        return a + b
+
+    @classmethod
+    def overlap(_, a, b):
+        return a + b
---
+++ test.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+from border import BorderOffset, BorderReweight
+from layers import Conv2D
+import unittest
+
+class TestBorderReweight(unittest.TestCase):
+    def check(self, shape, width, stride, acc=4):
+        border = BorderReweight(width, stride)
+        kernel = tf.ones(width + (1, 1)) / tf.cast(
+            tf.reduce_prod(width), tf.float32)
+        res = border(tf.convert_to_tensor(shape))
+        correct = 1 / tf.nn.conv2d(
+            tf.ones((1,) + shape + (1,)), kernel, (1,) + stride + (1,),
+            "SAME")[0, ..., 0]
+        self.assertEqual(res.shape, correct.shape)
+        diff = tf.reduce_max(res - correct)
+        self.assertAlmostEqual(diff.numpy().item(), 0, acc)
+
+    def test_basic(self):
+        self.check((16, 16), (8, 8), (1, 1))
+
+    def test_overlap(self):
+        self.check((5, 5), (8, 8), (1, 1))
+
+    def test_overflow(self):
+        self.check((5, 5), (16, 16), (1, 1))
+
+    def test_stride(self):
+        self.check((16, 16), (8, 8), (3, 2))
+
+    def test_overlap_stride(self):
+        self.check((5, 5), (8, 8), (3, 2))
+
+    def test_overflow_stride(self):
+        self.check((5, 5), (16, 16), (3, 2))
+
+    def test_long_stride(self):
+        self.check((16, 16), (8, 8), (30, 20))
+
+    def test_random(self):
+        tf.random.set_seed(0)
+        # tf.config.run_functions_eagerly(True)
+        for i in range(100):
+            inp = tf.cast(tf.random.uniform((3, 2)) * 32 + 1, tf.int32)
+            inp = tuple(map(tuple, inp))
+            self.check(*inp, acc=2)
+        # tf.config.run_functions_eagerly(False)
+
+class MockBorderOffset(BorderOffset):
+    def __init__(self, width, *args, **kwargs):
+        self.width = width
+        super().__init__(width, *args, **kwargs)
+
+    def initialize(self, size, width, axis, end):
+        return tf.ones((size,)) * self.width[1 - axis]
+
+class TestBorderOffset(unittest.TestCase):
+    def check(self, shape, width, stride):
+        border = MockBorderOffset(width, stride)
+        kernel = tf.ones(width + (1, 1))
+        res = border(shape)
+        correct = tf.reduce_sum(kernel) - tf.nn.conv2d(
+            tf.ones((1,) + shape + (1,)), kernel, (1,) + stride + (1,),
+            "SAME")[0, ..., 0]
+        self.assertEqual(res.shape, correct.shape)
+        print(res.numpy(), correct.numpy(), sep="\n\n")
+        self.assertEqual(tf.reduce_all(res == correct), True)
+
+    # removed because known to double count
+    def _test(self):
+        self.check((10, 10), (8, 5), (1, 1))
+
+class TestConvLayer(unittest.TestCase):
+    def test(self):
+        self.assertEqual(True, tf.reduce_all(Conv2D(
+            4, 3, 2, "same", kernel_initializer="ones")(
+                tf.ones((1, 5, 5, 1))) == 9))
+
+if __name__ == '__main__':
+    unittest.main()
